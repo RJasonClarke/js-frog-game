8d18155a71d067fe760d56be18c2a1068570b6bf
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-03-30 10:39:53 -0500

    added 1 second interval

--- frogs.js
+++ frogs.js
@@ -2,7 +2,17 @@ const score = {
     frogs: 1
 }
 
+setInterval(() => {
+    console.log("tick")
+}, 1000); //1000ms = 1 second
+
 increment = () => {
     document.getElementById("score").innerHTML = score.frogs
     score.frogs += 1
+}
+
+autoClick = () => {
+    if(score.frogs >= 5){
+        score.frogs
+    }
 }
