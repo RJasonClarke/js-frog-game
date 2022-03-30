0ca5ea1956d690e4f7da8862c39e393b0f638b99
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-03-30 12:27:20 -0500

    added buyCatchers that depletes amount of frogs and adds a catcher

--- frogs.js
+++ frogs.js
@@ -1,18 +1,23 @@
 const score = {
-    frogs: 1
+    frogs: 0,
+    catchers: 0
 }
+
+// increase frogs per interval based off of catchers and refresh frogs counter after purchase of a catcher
 
 setInterval(() => {
     console.log("tick")
 }, 1000); //1000ms = 1 second
 
 increment = () => {
-    document.getElementById("score").innerHTML = score.frogs
+    document.getElementById("score").innerHTML = score.frogs + 1
     score.frogs += 1
 }
 
-autoClick = () => {
+buyCatchers = () => {
     if(score.frogs >= 5){
-        score.frogs
+        document.getElementById("catchers").innerHTML = score.catchers + 1
+        score.catchers += 1
+        score.frogs -= 5
     }
 }
